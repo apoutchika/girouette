e86e6877677376beb19ba4272c7d0d7562aa61a5
slepinard <slepinard@sevanova.com>
2020-07-08 14:34:53 +0200

    :lipstick: Certificate popin

--- front/src/components/PopinCertificate.js
+++ front/src/components/PopinCertificate.js
@@ -1,27 +1,73 @@
-import React from 'react'
+import React, { Fragment } from 'react'
+import SVG from './SVG'
 
 const ENDPOINT = `https://girouette.devel`
 
 class PopinCertificate extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      copied: false
+    }
+  }
+
+  clipboard(url) {
+    navigator.clipboard.writeText(url)
+
+    this.setState({copied: true})
+
+    setTimeout(() => {
+      this.setState({copied: false})
+    }, 5000);
+  } 
+
   render() {
     const { active, toggleCertifPopin } = this.props
+    const { copied } = this.state
 
     return (
-      <div className={`certif-popin ${active && 'is-active'}`}>
-        <div className="certif-popin__container">
-          <button onClick={ toggleCertifPopin }>Close</button>
-          <br /><br />
+      <Fragment>
+        <div className={`certif-popin ${active && 'is-active'}`}>
+          <div className="certif-popin__container">
+            <div className="certif-popin__header">
+              <h2 className="certif-popin__title">
+                Certificate
+              </h2>
 
-          <a href={ENDPOINT.replace(/^https/, 'http') + '/certificate'} download>
-            Download certificate
-          </a>
+              <button
+                onClick={ toggleCertifPopin }
+                className="certif-popin__close"
+              >
+                <SVG icon="close" />
+              </button>
+            </div>
 
-          <ul>
-            <li>Chrome: chrome://settings/certificates</li>
-            <li>Firefox: about:preferences#privacy</li>
-          </ul>
+            <div className="text-center">
+              <a
+                className="certif-popin__download btn btn--rich"
+                href={ENDPOINT.replace(/^https/, 'http') + '/certificate'}
+                download
+              >
+                <SVG icon="download" extraClass="small-icon" />
+                Download
+              </a>
+            </div>
+
+            <span onClick={() => this.clipboard('chrome://settings/certificates') } className="label label--chrome">
+              <SVG icon="chrome" extraClass="small-icon" />
+              chrome://settings/certificates
+            </span>
+
+            <span onClick={() => this.clipboard('about:preferences#privacy') } className="label label--firefox">
+              <SVG icon="firefox" extraClass="small-icon" />
+              about:preferences#privacy
+            </span>
+          </div>
         </div>
-      </div>
+        
+        { copied && <div className="toast">Copied to clipboard</div> }
+      </Fragment>
     )
   }
 }
