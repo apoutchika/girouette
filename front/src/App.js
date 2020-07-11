2112a9acfe6eef7a11c64a5a6c24ced8f3b1ef92
Julien Philippon <juphilippon@gmail.com>
2020-07-11 19:03:11 +0200

    Create footer

--- front/src/App.js
+++ front/src/App.js
@@ -176,6 +176,29 @@ class App extends React.Component {
             })}
         </ul>
 
+        <footer className="footer">
+          <ul>
+            <li>
+              <a
+                href="https://www.girouette.dev"
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                Girouette.dev
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://github.com/apoutchika/girouette"
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                Github
+              </a>
+            </li>
+          </ul>
+        </footer>
+
         <PopinCertificate
           active={certifPopin}
           endpoint={ENDPOINT}
