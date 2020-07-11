1b10eb41df9d801aca5098b96e4f0ff554c35e28
Julien Philippon <juphilippon@gmail.com>
2020-07-11 20:34:04 +0200

    Add license check before install

--- girouette.sh
+++ girouette.sh
@@ -1,5 +1,33 @@
 #!/bin/bash
 
+echo ''
+echo '     _______                       __  __     '
+echo '    / ____(_)________  __  _____  / /_/ /____ '
+echo '   / / __/ / ___/ __ \/ / / / _ \/ __/ __/ _ \'
+echo '  / /_/ / / /  / /_/ / /_/ /  __/ /_/ /_/  __/'
+echo '  \____/_/_/   \____/\__,_/\___/\__/\__/\___/ '
+echo '  ____________________________________________'
+echo ''
+echo '        - https://www.girouette.dev -         '
+echo ''
+
+curl -X GET https://raw.githubusercontent.com/apoutchika/girouette/master/license
+echo ''
+echo ''
+
+
+ACCEPT='null'
+
+while [[ ${ACCEPT} != 'Y' && ${ACCEPT} != 'n' ]]
+do
+  echo -e "\e[1m"
+  read -p "? Accept the license ? [Y/n] : " ACCEPT
+  echo -e "\e[0m"
+  [[ ${ACCEPT} != 'Y' && ${ACCEPT} != 'n' ]] && echo -e "\e[31mPlease set Y or n...\e[0m"
+done
+
+[[ ${ACCEPT} != 'Y' ]] && echo ";-(" && exit
+
 
 echo ""
 echo -e "\e[34m> Test if has docker\e[39m\n"
